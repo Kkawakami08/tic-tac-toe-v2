65623b587fbfc37ec1a4e95c02ff785540218eb7
kkawakami08 <k.kawakami08@gmail.com>
2022-04-26 19:37:22 -0700

    playRound function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,52 @@
+let computerSelection;
+
 function computerPlay() {
   const computerArray = ["rock", "paper", "scissors"];
-  let computerSelection =
+  computerSelection =
     computerArray[Math.floor(Math.random() * computerArray.length)];
   return computerSelection;
 }
 
 console.log(computerPlay());
+
+let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+console.log(playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  let result;
+  if (computerSelection == "rock") {
+    if (playerSelection == "rock") {
+      return (result = "It's a tie!");
+    } else if (playerSelection == "paper") {
+      return (result = "You win!");
+    } else if (playerSelection == "scissors") {
+      return (result = "You lose!");
+    } else {
+      result = "Enter a valid choice";
+    }
+  } else if (computerSelection == "paper") {
+    if (playerSelection == "rock") {
+      return (result = "You lose!");
+    } else if (playerSelection == "paper") {
+      return (result = "It's a tie!");
+    } else if (playerSelection == "scissors") {
+      return (result = "You win!");
+    } else {
+      result = "Enter a valid choice";
+    }
+  } else if (computerSelection == "scissors") {
+    if (playerSelection == "rock") {
+      return (result = "You win!");
+    } else if (playerSelection == "paper") {
+      return (result = "You lose!");
+    } else if (playerSelection == "scissors") {
+      return (result = "It's a tie!");
+    } else {
+      result = "Enter a valid choice";
+    }
+  }
+  return result;
+}
+
+console.log(playRound(playerSelection, computerSelection));
